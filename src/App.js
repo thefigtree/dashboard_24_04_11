6e82490ede26f5111e3724cffddb71bd9b0b52a9
thefigtree <ldj8431@naver.com>
2024-04-11 22:42:37 +0900

    Background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="AppGlass"></div>
+    </div>
+  );
 }
 
 export default App;
