4171119dedf4dced7529c87940e2306ced139d37
thefigtree <ldj8431@naver.com>
2024-04-14 00:21:28 +0900

    func: CompactCard, ExpandedCard

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { LayoutGroup } from "framer-motion";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { UilTimes } from "@iconscout/react-unicons";
 import "./Card.css";
 
 const Card = (props) => {
@@ -7,21 +10,64 @@ const Card = (props) => {
 
   return (
     <LayoutGroup>
-      {expanded ? "Expanded" : <CompactCard params={props}></CompactCard>}
+      {expanded ? (
+        <ExpandedCard
+          params={props}
+          setExpanded={() => setExpanded(false)}
+        ></ExpandedCard>
+      ) : (
+        <CompactCard
+          params={props}
+          setExpanded={() => setExpanded(true)}
+        ></CompactCard>
+      )}
     </LayoutGroup>
   );
 };
 
 // CompactCard
-function CompactCard({ params }) {
+function CompactCard({ params, setExpanded }) {
   return (
-    <div className="CompactCard">
-      <div className="radialBar">Chart</div>
+    <div
+      className="CompactCard"
+      style={{
+        background: params.color.backGround,
+        boxShadow: params.color.boxShadow,
+      }}
+      onClick={setExpanded}
+    >
+      <div className="radialBar">
+        <CircularProgressbar
+          value={params.barValue}
+          text={`${params.barValue}%`}
+        ></CircularProgressbar>
+        <span>{params.title}</span>
+      </div>
 
       <div className="detail">
         <span>{params.value}</span>
         <span>Last 24 Hours</span>
       </div>
+    </div>
+  );
+}
+
+// ExpandedCard
+function ExpandedCard({ params, setExpanded }) {
+  return (
+    <div
+      className="ExpandedCard"
+      style={{
+        background: params.color.backGround,
+        boxShadow: params.color.boxShadow,
+      }}
+    >
+      <div>
+        <UilTimes onClick={setExpanded}></UilTimes>
+      </div>
+      <span>{params.title}</span>
+      <div className="chartContainer">Chart</div>
+      <span>Last 24 Hours</span>
     </div>
   );
 }
