7bd05c2f7835faf2b1bbaa7040f36f3d8897013a
thefigtree <ldj8431@naver.com>
2024-04-15 15:13:37 +0900

    Bring Mui Table

--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./MainDash.css";
 import Cards from "../Cards/Cards";
+import Table from "../Table/Table";
 
 const MainDash = () => {
   return (
     <div className="MainDash">
       <h1>Portfolio</h1>
       <Cards></Cards>
+      <h2>Portfolio</h2>
+      <Table></Table>
     </div>
   );
 };
