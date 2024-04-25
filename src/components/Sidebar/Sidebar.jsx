f981a23f3d6e24a56990db2b723f18d63b71f306
thefigtree <ldj8431@naver.com>
2024-04-25 17:22:02 +0900

    Mediaquery

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,31 +1,56 @@
 import React, { useState } from "react";
 import "./Sidebar.css";
 import { SidebarData } from "../../Data/Data";
+import { UilBars } from "@iconscout/react-unicons";
+import { motion } from "framer-motion";
 
 const Sidebar = () => {
   const [selected, setSelected] = useState(0);
+  const [expanded, setExpanded] = useState(true);
+
+  const sidebarVariants = {
+    true: {
+      left: "0",
+    },
+    false: {
+      left: "-60%",
+    },
+  };
 
   return (
-    <div className="Sidebar">
-      <div className="logo">
-        <span>이동진</span>
+    <>
+      <div
+        className="bars"
+        style={expanded ? { left: "60%" } : { left: "5%" }}
+        onClick={() => setExpanded(!expanded)}
+      >
+        <UilBars></UilBars>
       </div>
+      <motion.div
+        className="Sidebar"
+        variants={sidebarVariants}
+        animate={window.innerWidth <= 768 ? `${expanded}` : ""}
+      >
+        <div className="logo">
+          <span>이동진</span>
+        </div>
 
-      <div className="menu">
-        {SidebarData.map((item, index) => {
-          return (
-            <div
-              className={selected === index ? "menuItem active" : "menuItem"}
-              key={index}
-              onClick={() => setSelected(index)}
-            >
-              <item.icon></item.icon>
-              <span>{item.title}</span>
-            </div>
-          );
-        })}
-      </div>
-    </div>
+        <div className="menu">
+          {SidebarData.map((item, index) => {
+            return (
+              <div
+                className={selected === index ? "menuItem active" : "menuItem"}
+                key={index}
+                onClick={() => setSelected(index)}
+              >
+                <item.icon></item.icon>
+                <span>{item.title}</span>
+              </div>
+            );
+          })}
+        </div>
+      </motion.div>
+    </>
   );
 };
 
